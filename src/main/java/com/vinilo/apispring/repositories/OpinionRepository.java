2b3d2fdd786a741028d687eadf88fa9c2cb49ad9
Luis Daniel Ortega Pedrajas <alortega@iesfranciscodelosrios.es>
2021-03-02 00:13:39 +0100

    errores resueltos

--- src/main/java/com/vinilo/apispring/repositories/OpinionRepository.java
+++ src/main/java/com/vinilo/apispring/repositories/OpinionRepository.java
@@ -11,6 +11,6 @@ import com.vinilo.apispring.model.Opinion;
 @Repository
 public interface OpinionRepository extends JpaRepository<Opinion, Long>{
 	
-	@Query(value=" select t2.* from ( select * from product where id = ?1 ) t1 left join ( select id, id_user, id_product, image, description from opinion ) t2 on t1.id = t2.id_product ")
+	@Query(value="select t1.id as id_product, t2.id, t2.id_user, t2.image, t2.description from ( select * from product where id = ?1 ) t1 left join ( select id, id_user, id_product, image, description from opinion ) t2 on t1.id = t2.id_product", nativeQuery=true)
 	public List<Opinion>getOpinionByProduct(Long id);
 }
